30f33667774bb94b5800e4fe7b4c9e1106ac06da
Rishabh Jain <rishabh.jain@mindtickle@com>
2022-11-19 21:09:41 +0530

    feat(docs): added docs

--- jest.config.ts
+++ jest.config.ts
@@ -162,7 +162,7 @@ export default {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-    "/node_modules/", '/lib/'
+    "/node_modules/", '/lib/', '/docs/'
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
